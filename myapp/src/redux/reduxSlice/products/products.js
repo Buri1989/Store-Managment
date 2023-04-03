4608a3cd9d0692ba28a76da8ffac60f211745fe3
Boris Sverdlov <Borkya@gmail.com>
2023-04-03 20:32:38 +0300

    Create products data

---
+++ myapp/src/redux/reduxSlice/products/products.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const initialState = [
+    {
+        ID: '1',
+        Name: 'Air Jordan3',
+        Price: 900,
+        Quantity: '23'
+    },
+    {
+        ID: '2',
+        Name: 'Air Jordan1',
+        Price: 1500,
+        Quantity: '10'
+    },
+    {
+        ID: '3',
+        Name: 'Air Jordan4',
+        Price: 900,
+        Quantity: '150'
+    },
+]
+
+const products = createSlice({
+    name: 'products',
+    initialState,
+    reducers: {
+        addProduct(state, action) {
+            return state.concat(action.payload)
+        },
+        updateProduct(state, action) {
+            let product = [...state]
+            product = product.map(index => {
+                if (Number(index.ID) === Number(action.payload.ID)) {
+                    return action.payload
+                } else {
+                    return index
+                }
+            })
+            return product
+        },
+        deleteProduct(state, action) {
+            const start = [...state.slice(0, action.payload - 1)]
+            const product = [...state.slice(action.payload)]
+
+            return start.concat(product)
+        }
+    }
+})
+
+export const { addProduct, updateProduct, deleteProduct } = products.actions
+
+export default products.reducer
