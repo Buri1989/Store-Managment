90403a9345c526029d827856db614c79e5437401
Boris Sverdlov <Borkya@gmail.com>
2023-04-06 11:02:16 +0300

    Update and finish user table comp

--- myapp/src/components/products/product/expended Zone/Region2/UsersTableComp.js
+++ myapp/src/components/products/product/expended Zone/Region2/UsersTableComp.js
@@ -80,11 +80,138 @@ TablePageAction.propTypes = {
     onPageChange: PropTypes.func.isRequired,
 };
 
-//TODO: Finish styling the table
+const StylingTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
+}));
 
 
-const UsersTableComp = (props) => {
-    //TODO:Finish the user table comp
+const UsersTableComp = ({ productID, customersSelect, purchasesSelect }) => {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [myRows, setMyRows] = useState([]);
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        let tempRows = [];
+        if (purchasesSelect) {
+            const productPurchases = purchasesSelect.filter(purchase => {
+                return purchase.ProductId === productID + ''
+            });
+            if (productPurchases) {
+                productPurchases.forEach(item => {
+                    const cus = customersSelect.filter(cus => {
+                        return cus.ID === item.CustomerId
+                    });
+                    if (cus) {
+                        tempRows.push({
+                            customerID: cus.ID,
+                            name: cus.FirstName + " " + cus.LaseName,
+                            purchaseDate: item.Date,
+                            Add: "Add"
+                        })
+                    }
+                })
+            }
+            setMyRows(tempRows)
+        }
+    }, [productID, customersSelect, purchasesSelect]);
+
+    /*Avoid a layout jump when reaching the last page with empty rows. */
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - myRows.length) : 0;
+
+    const handlePageChange = (event, newPage) => {
+        setPage(parseInt(newPage, 10));
+    };
+
+    const handleRowsPageChange = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const theme = useTheme();
+
+    return (
+        <ThemeProvider theme={theme}>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 500 }} aria-label='custom pagination table'>
+                    <TableHead>
+                        <TableRow>
+                            <StylingTableCell>Name</StylingTableCell>
+                            <StylingTableCell align='center'>Purchase Date</StylingTableCell>
+                            <StylingTableCell align='center'>Add New Product To Customer</StylingTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {(rowsPerPage > 0
+                            ? myRows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            : myRows
+                        ).map((row) => (
+                            <TableRow key={row.name} sx={{
+                                color: 'white',
+                                '&': {
+                                    'a': {
+                                        color: 'white'
+                                    }
+                                },
+                            }}>
+                                <TableCell component="th" scope="row">
+                                    <Link to={`/EditCustomers/${row.customerID}`}>{row.name}</Link>
+                                </TableCell>
+                                <TableCell style={{ width: 160 }} align="center">
+                                    {row.purchaseDate}
+                                </TableCell>
+                                <TableCell style={{ width: 260 }} align="center">
+                                    <Box sx={{
+                                        flexGrow: 1,
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        textAlign: 'center'
+                                    }}>
+                                        <Button variant="contained" color="success" endIcon={<SendIcon />} onClick={() => {
+                                            navigate(`/BuyProduct/customerId=${row.customerID}`)
+                                        }}>{row.Add}</Button>
+                                    </Box>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                        {emptyRows > 0 && (
+                            <TableRow style={{ height: 53 * emptyRows }}>
+                                <TableCell colSpan={6} />
+                            </TableRow>
+                        )}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                colSpan={3}
+                                count={myRows.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                SelectProps={{
+                                    inputProps: {
+                                        'aria-label': 'rows per page',
+                                    },
+                                    native: true,
+                                }}
+                                onPageChange={handlePageChange}
+                                onRowsPerPageChange={handleRowsPageChange}
+                                ActionsComponent={TablePageAction}
+                            />
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </TableContainer>
+        </ThemeProvider>
+    )
 }
 
 export default UsersTableComp
